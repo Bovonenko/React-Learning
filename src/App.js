315bf2b36c8bc5e134118467f384b66b0f1ee014
Rostyslav <bovonenko@icloud.com>
2022-07-10 13:04:16 +0200

    Components STATE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++'
+		}
+	}
+
+	nextYear = () => {
+		console.log('+++');
+		const num = 4;
+		this.setState(state => ({
+			years: state.years = num
+		}))
+	}
+
+	render() {
+		const {name, surname, link} = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>My profile</a>
+			</div>
+		)
+	}
+}
+
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <WhoAmI name='John' surname="Smith" link="facebook.com"/>
+        <WhoAmI name='Alex' surname="Sadzc" link="vk.com"/>
     </div>
-  );
+  )
 }
 
 export default App;
